c79c7ad2f719bfb326b68cef05d3e1100d2dab4d
8simsim8 <8simsim8@gmail.com>
2016-09-09 18:44:18 +0300

    Исправлена возможность стоп всего скрола

--- iOS - scroll/iOS-scroll.js
+++ iOS - scroll/iOS-scroll.js
@@ -1,30 +1,30 @@
-// scrollBlock - block-modal window, what scroll. If there no element, all document don`t scroll
+// scrollBlock - block-modal window, what scroll
 // stopScroll - block, what disable scroll
-
 function iOSscroll(scrollBlock, stopScroll) {
 
 	var elemEnableScroll = searchElement(scrollBlock);
 
-	if(elemEnableScroll !== null) {
-	// disable scroll if it out scroll element
-		elemEnableScroll.addEventListener('touchmove', function(e){
-			var elemToch = e.target; // touch element
-			var isEnableScroll = false;
-			while(elemToch !== null) {
-				if(elemEnableScroll != elemToch) {
-					isEnableScroll = true; // if touch element - children of scroll block
-					break;
-				}
-				elemToch = elemToch.parentNode;
+// disable scroll if it out scroll element
+if(elemEnableScroll !== null) {
+	elemEnableScroll.addEventListener('touchmove', function(e){
+		var elemToch = e.target; // touch element
+		var isEnableScroll = false;
+		while(elemToch !== null) {
+			if(elemEnableScroll != elemToch) {
+				isEnableScroll = true; // if touch element - children of scroll block
+				break;
 			}
-			if(!isEnableScroll) {
-				e.preventDefault();
-			}
-			e.stopPropagation();
-		}, false);
-	// remove effect Rubber in no-scroll window on iOS
-		removeIOSRubberEffect( elemEnableScroll );
-	}
+			elemToch = elemToch.parentNode;
+		}
+		if(!isEnableScroll) {
+			e.preventDefault();
+		}
+		e.stopPropagation();
+	}, false);
+
+// remove effect Rubber in no-scroll window on iOS
+	removeIOSRubberEffect( elemEnableScroll );
+}
 
 // if modal window is open - disable scroll page
 	window.addEventListener('touchmove', function(e) {
